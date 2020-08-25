04bcbe20a4d8fc186669264559aa33302a43c7d6
Melanie <mel.lontsi@gmail.com>
2020-08-24 23:57:51 -0700

    totally refactored the project to accept csv files

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,38 +1,76 @@
-/**
-  Sign up Form Validation
-*/
+const firebaseConfig = {
+  apiKey: "AIzaSyDl-1oy6ekvb_6CzfZmQd61WyoWuQBiEjA",
+  authDomain: "project-pdd-registration.firebaseapp.com",
+  databaseURL: "https://project-pdd-registration.firebaseio.com",
+  projectId: "project-pdd-registration",
+  storageBucket: "project-pdd-registration.appspot.com",
+  messagingSenderId: "125332366348",
+  appId: "1:125332366348:web:5fbf9fd52952487506fa29",
+  measurementId: "G-ZGGGVZ2ZW4",
+};
 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
-$('#inputPassword').on('input', function(){
-  if ($(this).val().length >= 6){
-    $('#pwd-has-error').remove();
+/*** migrate to firebase */
+var migrationProgress = $("#migration-progress");
+var migrationProgressInfo = $("#migration-progress-info");
+
+$("#btn-migrate-to-firebase").on("click", function () {
+  d3.csv("/uploads/database.csv").then(function (data) {
+    var percentage = 0;
+    for (var i = 0; i < data.length; i++) {
+      var doc = data[i];
+      db.collection("retraitants")
+        .add(doc)
+        .then(function () {
+          console.log(`${doc} is imported successfully to firestore!`);
+          percentage = (i / data.length) * 100;
+          migrationProgress.attr(
+            "style",
+            "width: " + Math.ceil(percentage) + "%"
+          );
+          migrationProgress.attr("aria-valuenow", Math.ceil(percentage));
+        })
+        .catch(function (error) {
+          console.error("Error writing document: ", error);
+        });
+    }
+  });
+});
+
+$("#inputPassword").on("input", function () {
+  if ($(this).val().length >= 6) {
+    $("#pwd-has-error").remove();
   }
 });
 
-$('#inputPasswordConfirm').on('input', function(){
-  if ($('#inputPassword').val() == $(this).val()) {
-    $('#pwd-must-match').remove();
+$("#inputPasswordConfirm").on("input", function () {
+  if ($("#inputPassword").val() == $(this).val()) {
+    $("#pwd-must-match").remove();
   }
-
 });
 
-$('#inputPassword').on('focusin', function(){
-  $(this).addClass('has-focus');
-  if($(this).val().length<5)
-    $(this).after('<span class="text-danger font-weight-light" id="pwd-has-error">Password must be at least 6 characters long</span>');
-
+$("#inputPassword").on("focusin", function () {
+  $(this).addClass("has-focus");
+  if ($(this).val().length < 5)
+    $(this).after(
+      '<span class="text-danger font-weight-light" id="pwd-has-error">Password must be at least 6 characters long</span>'
+    );
 });
-$('#inputPasswordConfirm').on('focusin', function(){
-  $(this).addClass('has-focus');
-  if ($('#inputPassword').val() != $(this).val()) {
-    $(this).after('<span class="text-danger font-weight-light" id="pwd-must-match">Your passwords must match</span>');
-  }
 
+$("#inputPasswordConfirm").on("focusin", function () {
+  $(this).addClass("has-focus");
+  if ($("#inputPassword").val() != $(this).val()) {
+    $(this).after(
+      '<span class="text-danger font-weight-light" id="pwd-must-match">Your passwords must match</span>'
+    );
+  }
 });
 /*
  Upload File Input
 */
-$(".custom-file-input").on("change", function() {
+$(".custom-file-input").on("change", function () {
   var fileName = $(this).val().split("\\").pop();
   $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,124 +1,106 @@
-const express = require('express');
-const process = require('process');
-const path = require('path');
-const fs = require('fs');
-const Busboy = require('busboy');
-const auth = require('../services/auth.js');
-const database = require('../services/database.js');
+const express = require("express");
+const process = require("process");
+const path = require("path");
+const fs = require("fs");
+const Busboy = require("busboy");
+const auth = require("../services/auth.js");
 
 const router = express.Router();
 const user = new auth();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
+router.get("/", function (req, res, next) {
   if (req.session.loggedIn) {
-    res.render('admin', {
-      title: 'Home',
+    res.render("admin", {
+      title: "Home",
       isSignedIn: true,
-      adminName: ` back ${req.session.useremail}`
+      adminName: ` back ${req.session.useremail}`,
     });
   } else {
-    res.render('index');
+    res.render("index");
   }
-
 });
 
-
-router.get('/admin', function(req, res, next) {
-
-    if(req.session.loggedIn){
-      res.render('admin', {
-        title: 'Home',
-        isSignedIn: true,
-        adminName: ` back ${req.session.useremail}`
-      });
-    }
-    else{
-      res.render('index');
-    }
-
-});
-
-
-router.post('/admin', function(req, res, next) {
-
-  user.signin(req.body.inputEmail, req.body.inputPassword).then(credentials => {
-
-    req.session.loggedIn = true;
-    req.session.useremail = credentials.user.email;
-
-    res.render('index', {
-      title: 'Home',
+router.get("/admin", function (req, res, next) {
+  res.render("admin", {
+    title: "Home",
+    isSignedIn: true,
+    adminName: ` back ${req.session.useremail}`,
+  });
+  /*
+  if (req.session.loggedIn) {
+    res.render("admin", {
+      title: "Home",
       isSignedIn: true,
-      adminName: credentials.user.email
+      adminName: ` back ${req.session.useremail}`,
     });
-  }).catch(err => {
-    res.render('error', {
-      message: err.code,
-      error: {
-        status: 404,
-        stack: err.message
-      }
-    });
-  });
-
+  } else {
+    res.render("index");
+  } */
 });
 
+router.post("/admin", function (req, res, next) {
+  user
+    .signin(req.body.inputEmail, req.body.inputPassword)
+    .then((credentials) => {
+      req.session.loggedIn = true;
+      req.session.useremail = credentials.user.email;
 
-router.get('/logout', function(req, res, next) {
+      res.render("index", {
+        title: "Home",
+        isSignedIn: true,
+        adminName: credentials.user.email,
+      });
+    })
+    .catch((err) => {
+      res.render("error", {
+        message: err.code,
+        error: {
+          status: 404,
+          stack: err.message,
+        },
+      });
+    });
+});
+
+router.get("/logout", function (req, res, next) {
   user.signout();
-  req.session.destroy(err => {
-    console.log("error occured while clearing the session")
+  req.session.destroy((err) => {
+    console.log("error occured while clearing the session");
   });
-  res.redirect('/');
-
+  res.redirect("/");
 });
 
-
-router.post('/upload', function(req, res, next) {
-  const fsInstance = new database();
-
-  var uploadPath = path.join(process.cwd(), 'uploads');
+router.post("/upload", function (req, res, next) {
+  var uploadPath = path.join(process.cwd(), "public/uploads/database.csv");
   var busboy = new Busboy({
-    headers: req.headers
+    headers: req.headers,
   });
 
-  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-
-    var saveTo = path.join(uploadPath, path.basename(filename));
-
-    file.on('data', function(data) {
-      console.log('File [' + filename + '] was successfully uploaded...');
+  busboy.on("file", function (fieldname, file, filename, encoding, mimetype) {
+    file.on("data", function (data) {
+      console.log("File [" + filename + "] was successfully uploaded...");
     });
 
-    const writeStream = fs.createWriteStream(saveTo);
+    const writeStream = fs.createWriteStream(uploadPath);
     file.pipe(writeStream);
 
-    file.on('end', () => writeStream.end());
+    file.on("end", () => writeStream.end());
 
-    writeStream.on('finish', () => {
-      console.log("Import into Firebase will start now...");
-      fsInstance.import(saveTo);
-    });
-    writeStream.on('error', (err) => {
-      console.log("there was an error");
-	console.log(err);
+    writeStream.on("error", (err) => {
+      console.log("there was an error: " + err);
     });
   });
 
-  busboy.on('finish', function() {
-    res.render('confirmation', {
+  busboy.on("finish", function () {
+    res.render("confirmation", {
       isSignedIn: true,
-      message: 'Data uploaded successfully. It can take few minutes for fully create all Documents in Database'
-
+      message:
+        "File saved successfully. You can now start migration. <br> It can take few minutes for fully create all Documents in Database <br> See Dev Console for more details",
     });
-
   });
 
   req.pipe(busboy);
-
 });
-
 
 module.exports = router;
